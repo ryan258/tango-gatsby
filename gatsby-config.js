368f483ca949427f52e80bb54702208472ef4fc4
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-05-06 11:07:24 -0500

    Add project gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,33 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Oswald`,
+            subsets: [`latin`],
+          },
+          {
+            family: `Open Sans`,
+            variants: [`400`, `700`],
+          },
+        ],
+      },
+    },
+    // {
+    //   resolve: `gatsby-source-wordpress`,
+    //   options: {
+    //     // url: ``
+    //   },
+    // },
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
